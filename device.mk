f6c27e83a32d74d36efeac7bca80a83ec7dce368
dianlujitao <dianlujitao@users.noreply.github.com>
2019-12-31 14:05:40 +0000

    potter: Build LiveDisplay HAL

--- device.mk
+++ device.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Display Calibration
+ PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-qdcm
+
 # DRM
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite \
